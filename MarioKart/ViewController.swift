737b649059c0ef2aa5d2c9d565a0cbd7249407a1
moldingtofu <jeremykchang@gmail.com>
2019-02-06 19:07:40 -0500

    pan, pinch, rotate gestures

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let kartView = sender.view!
+        kartView.center = location
+        
+    }
+    
+    @IBAction func didPinchKartView(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+    }
+    
+    @IBAction func didRotateKartView(_ sender: UIRotationGestureRecognizer) {
+        let rotation = sender.rotation
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(rotationAngle: rotation)
+    }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Mario Kart
-App to practice swift animations.
----
+# *Mario Kart*
+
+**Mario Kart** is a basic app to test animations and gestures.
+
+Time spent: **0** hours spent in total
+
+## User Stories - Part I
+
+- [x] User can move karts.
+- [x] User can pinch karts to shrink kart.
+- [x] User can pinch-rotate karts to rotate kart.
